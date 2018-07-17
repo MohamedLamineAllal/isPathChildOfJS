0bb46d6174cd0d03bc0b709d3f682742423edc9e
Allal Mohamed Lamine <allalmohmaedlamine@gmail.com>
2018-07-17 16:01:42 +0100

    minim changes

--- comparingStringsMethod/isPathChildOf.js
+++ comparingStringsMethod/isPathChildOf.js
@@ -4,6 +4,7 @@ function isPathChildOf(path, parentPath) {
     path = path.trim();
     parentPath = parentPath.trim();
 
+    // trick: making sure the paths end with a separator
     let lastChar_path = path[path.length - 1];
     let lastChar_parentPath = path[parentPath.length - 1];
     if (lastChar_parentPath !== '\\' && lastChar_parentPath !== '/') parentPath += '/';
@@ -18,8 +19,6 @@ function isPathChildOf(path, parentPath) {
     if (parentPath.length >= path.length) return false; // parent path should be smaller in characters then the child path (and they should be all the same from the start , if they differ in one char then they are not related)
 
     for (let i = 0; i < parentPath.length; i++) {
-        // let sepaParent = ;
-        // let sepaChild = ;
         if (!(isPathSeparator(parentPath[i]) && isPathSeparator(path[i])) && parentPath[i] !== path[i]) {// if both are not separators, then we compare (if one is separator, the other is not, the are different, then it return false, if they are both no separators, then it come down to comparaison, if they are same nothing happen, if they are different it return false)
             return false;
         }
